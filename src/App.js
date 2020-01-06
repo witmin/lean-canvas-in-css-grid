383caef9105349abfc9601193ffb924ee1674cfe
Millie Lin <linxinmin@gmail.com>
2020-01-06 11:17:40 +0800

    Init the canvas with CSS Grid.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <div className="canvas">
+                <div className="first row">
+                    <div className="box one problem"><h2>Problem</h2></div>
+                    <div className="box two solution"><h2>Solution</h2></div>
+                    <div className="box three key-metrics"><h2>Key metrics</h2></div>
+                    <div className="box four uni-val-prop"><h2>Unique value proposition</h2></div>
+                    <div className="box five unfair-advantage"><h2>Unfair advantage</h2></div>
+                    <div className="box six channels"><h2>Channels</h2></div>
+                    <div className="box seven customer segments"><h2>Customer segments</h2></div>
+                </div>
 
-    </div>
-  );
+                <div className="second row">
+                    <div className="box eight cost-structure"><h2>Cost structure</h2></div>
+                    <div className="box nine revenue-streams"><h2>Revenue streams</h2></div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
