53274653aa1c2868716285e753ab802157b3471f
Millie Lin <linxinmin@gmail.com>
2020-01-06 12:08:20 +0800

    Tweaked style details.

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ function App() {
                         <p className="help">List your top 1-3 problems.</p>
                         <h3>Existing alternatives</h3>
                         <p className="help">List how these problems are solved today.</p>
-                        <div className="filling-order" title="Suggested filling order">1</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">1</div>
                     </div>
 
                     {/* Solution */}
                     <div className="box two solution">
                         <h2>Solution<span lang="zh-hans">解决方案</span></h2>
                         <p className="help">Outline a possible solution for each problem</p>
-                        <div className="filling-order" title="Suggested filling order">4</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">4</div>
                     </div>
 
                     {/* Key metrics */}
                     <div className="box three key-metrics">
                         <h2>Key metrics<span lang="zh-hans">关键指标</span></h2>
                         <p className="help">List the key numbers that tell you how your business is doing.</p>
-                        <div className="filling-order" title="Suggested filling order">8</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">8</div>
                     </div>
 
                     {/* Unique Value Proposition */}
@@ -35,7 +35,7 @@ function App() {
                         <p className="help">Single, clear, compelling message that states why you are different and worth paying attention.</p>
                         <h3>High-level concept</h3>
                         <p className="help">List your X for Y analogy e.g. YouTube = Flickr for videos.</p>
-                        <div className="filling-order" title="Suggested filling order">3</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">3</div>
 
                     </div>
 
@@ -43,14 +43,14 @@ function App() {
                     <div className="box five unfair-advantage">
                         <h2>Unfair advantage<span lang="zh-hans">压倒性优势</span></h2>
                         <p className="help">Something that cannot easily be bought or copied.</p>
-                        <div className="filling-order" title="Suggested filling order">9</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">9</div>
                     </div>
 
                     {/* Channels */}
                     <div className="box six channels">
                         <h2>Channels<span lang="zh-hans">渠道</span></h2>
                         <p className="help">List your path to customers (inbound or outbound).</p>
-                        <div className="filling-order" title="Suggested filling order">5</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">5</div>
                     </div>
 
                     {/* Customer segments */}
@@ -59,7 +59,7 @@ function App() {
                         <p className="help">List your target customers and users.</p>
                         <h3>Early adopters</h3>
                         <p className="help">List the characteristics of your ideal customers.</p>
-                        <div className="filling-order" title="Suggested filling order">2</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">2</div>
                     </div>
 
                 </div>
@@ -70,21 +70,21 @@ function App() {
                     <div className="box eight cost-structure">
                         <h2>Cost structure<span lang="zh-hans">成本结构</span></h2>
                         <p className="help">List your fixed and variable costs.</p>
-                        <div className="filling-order" title="Suggested filling order">7</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">7</div>
                     </div>
 
                     {/* Revenue stream*/}
                     <div className="box nine revenue-streams">
                         <h2>Revenue streams<span lang="zh-hans">收入流向</span></h2>
                         <p className="help">List your sources of revenue.</p>
-                        <div className="filling-order" title="Suggested filling order">6</div>
+                        <div className="filling-order" title="Suggested filling order 建议填写顺序">6</div>
                     </div>
 
                 </div>
             </div>
 
             {/*    Footer    */}
-            <footer>
+            <footer className="footer">
                 <p>Lean Canvas in CSS Grid was built by <a href="https://www.millielin.com">Millie Lin</a> referred to <a href="https://leanstack.com/leancanvas">Leanstack</a>.
                 </p>
                 <p className="copyright">Lean Canvas is adapted from The Business Model Canvas and is licensed under the Creative Commons Attribution-Share Alike 3.0 Un-ported License.</p>
