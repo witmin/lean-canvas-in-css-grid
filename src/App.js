f1660db6a084e323a0b1a90e9bceeb4d64db7c7c
Millie Lin <linxinmin@gmail.com>
2020-01-06 12:14:35 +0800

    Added Chinese for the sub-titles

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
                     <div className="box one problem">
                         <h2>Problem <span lang="zh-hans">问题</span></h2>
                         <p className="help">List your top 1-3 problems.</p>
-                        <h3>Existing alternatives</h3>
+                        <h3>Existing alternatives <span lang="zh-hans">现有替代方案</span></h3>
                         <p className="help">List how these problems are solved today.</p>
                         <div className="filling-order" title="Suggested filling order 建议填写顺序">1</div>
                     </div>
@@ -33,7 +33,7 @@ function App() {
                     <div className="box four uni-val-prop">
                         <h2>Unique value proposition<span lang="zh-hans">独特的价值主张</span></h2>
                         <p className="help">Single, clear, compelling message that states why you are different and worth paying attention.</p>
-                        <h3>High-level concept</h3>
+                        <h3>High-level concept <span lang="zh-hans">高级概念</span></h3>
                         <p className="help">List your X for Y analogy e.g. YouTube = Flickr for videos.</p>
                         <div className="filling-order" title="Suggested filling order 建议填写顺序">3</div>
 
@@ -57,7 +57,7 @@ function App() {
                     <div className="box seven customer segments">
                         <h2>Customer segments<span lang="zh-hans">客户群</span></h2>
                         <p className="help">List your target customers and users.</p>
-                        <h3>Early adopters</h3>
+                        <h3>Early adopters<span lang="zh-hans">早期采用者</span></h3>
                         <p className="help">List the characteristics of your ideal customers.</p>
                         <div className="filling-order" title="Suggested filling order 建议填写顺序">2</div>
                     </div>
